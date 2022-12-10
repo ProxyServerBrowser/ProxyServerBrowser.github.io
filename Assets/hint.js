07081ddc6d1262fbc43bb2bdc9307989c5daab45
Connor <107011324+connor33341@users.noreply.github.com>
2022-12-09 18:40:19 -0700

    Create hint.js

---
+++ Assets/hint.js
@@ -0,0 +1,14 @@
+function showHint(str) {
+  if (str.length == 0) {
+  return null;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       return this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "gethint.php?q=" + str, true);
+    xmlhttp.send();
+  }
+}
